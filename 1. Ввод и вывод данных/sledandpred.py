2d187c2ccb567d5a212b2c27d51816c88813a155
Sima Tarverdyan <102418053+twers1@users.noreply.github.com>
2022-05-05 20:09:29 +0300

    Create sledandpred.py

---
+++ 1. Ввод и вывод данных/sledandpred.py
@@ -0,0 +1,5 @@
+# Условие: Напишите программу, которая считывает целое число и выводит текст, аналогичный приведенному в примере (пробелы важны!).
+# Решение: 
+n = int(input())
+print('The next number for the number ' + str(n) + ' is ' + str(n+1) + '.')
+print('The previous number for the number ' + str(n) + ' is ' + str(n-1) + '.')
