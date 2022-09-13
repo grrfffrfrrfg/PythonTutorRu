d345b1c9d6f76fecff6696d5b82ba959756472a7
Sima Tarverdyan <102418053+twers1@users.noreply.github.com>
2022-09-13 19:44:28 +0300

    Update 2.отрицательнаястепень.py

--- 8. Функции и рекурсия/2.отрицательнаястепень.py
+++ 8. Функции и рекурсия/2.отрицательнаястепень.py
@@ -7,3 +7,18 @@ def power(a, n):
     p = a**n
     return(p)
 print(power(a, n))
+# Второй способ
+def power(a, n):
+    res = 1
+    for i in range(abs(n)):
+        res *= a 
+    if n<0:
+        return 1/res
+    else:
+        return res
+    
+    
+a = float(input())
+n = int(input())
+
+print(power(a, n))
